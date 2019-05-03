aafe068f4e6c035b12fbbecb7b54095165130157
Kenyon Yan <39213849+KenyonYan@users.noreply.github.com>
2019-05-03 14:37:12 +0800

    day2

---
+++ Gesture/Assets/Scripts/FollowModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowModel : MonoBehaviour {
+
+    private GameObject axixY;         
+    private Transform showModel;        //当前显示的模型
+    private Vector3 offset;
+
+    private bool isFirst = false;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        showModel = GameObject.Find("AxisY").GetComponentInChildren<Transform>();
+        offset = this.transform.position - showModel.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, showModel.transform.position+offset, Time.deltaTime*1.0f);
+    }
+}
---
+++ Gesture/Assets/Scripts/Xray/Drag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Drag : MonoBehaviour {
+
+    public Texture[] Lugu;
+    public Slider slider;
+    public RawImage xraySection;
+
+    private float sliderValue;
+    private Vector3 startPosition;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.GetComponent<RawImage>().texture = Lugu[0];
+        startPosition = xraySection.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        sliderValue = slider.value;
+        int index = (int)(sliderValue * (Lugu.Length - 1));
+        xraySection.transform.position = new Vector3(startPosition.x, startPosition.y+(float)index*0.01f , startPosition.z);
+        //print(index);
+        this.GetComponent<RawImage>().texture = Lugu[index];
+    }
+}
