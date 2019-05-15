ae7b2b04a31af0390eb484cd94843162e60849aa
Kenyon Yan <39213849+KenyonYan@users.noreply.github.com>
2019-05-15 14:59:20 +0800

    ..

--- Gesture/Assets/Scripts/VoiceTest.cs
+++ Gesture/Assets/Scripts/VoiceTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VoiceTest : MonoBehaviour {
 
     public GameObject gameObject;
+    bool isRotate = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +15,14 @@ public class VoiceTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (isRotate)
+        {
+            gameObject.transform.Rotate(new Vector3(0, 1, 0));
+        }
+    }
 
     public void TestSound()
     {
-        gameObject.transform.Rotate(new Vector3(0, 1, 0));
+        isRotate = !isRotate;
     }
 }
